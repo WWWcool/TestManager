4c87e5dba1ee68822106bbbffc299296650d765f
Артем <WWW_cool@inbox.ru>
2014-07-02 13:33:46 +0300

    testGit

---
+++ TimeManeger_v0.001/TimeManeger_v0.001/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimeManeger_v0._001
+{
+    public partial class f_Main : Form
+    {
+        public f_Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
